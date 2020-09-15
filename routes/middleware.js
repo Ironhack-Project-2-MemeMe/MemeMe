d0c285b34651860ac55b99ffe24430bb609073d0
Diana <d.thueroff@gmail.com>
2020-09-15 22:14:33 +0200

    login works

--- routes/middleware.js
+++ routes/middleware.js
@@ -1,13 +1,19 @@
-const loginCheck = () => {
-  return (req, res, next) => {
-    if (req.isAuthenticated()) {
-      next();
-    } else {
-      res.redirect('/login');
-    }
-  };
-};
-
+const User = require('../models/User'); 
+ 
+function loginCheck (req, res, next)  {
+  if (req.isAuthenticated()) {
+    console.log('Current user', req.session.passport.user);
+    User.findOne({ _id: req.session.passport.user })
+    .then(found => {
+      if (found !== null) {
+        req.mememeUser = found;
+        next();
+      } 
+    });
+  } else {
+    res.redirect('/login');
+  }
+}
 
 module.exports = {
   loginCheck: loginCheck
